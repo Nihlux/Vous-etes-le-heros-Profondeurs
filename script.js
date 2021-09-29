c1c93e4e72d963d2e087fe125c43ca86a5cf3be5
Snxww <90851816+Snxww@users.noreply.github.com>
2021-09-28 20:22:12 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const chapitre1 = {
+    subtitle: "titreChapitre1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    img: ""
+}
+
+const chapitre2 = {
+    subtitle: "titreChapitre2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    img: ""
+}
+
+const chapitre3 = {
+    subtitle: "titreChapitre3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    img: ""
+}
+
+const chapitre4 = {
+    subtitle: "titreChapitre4",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    img: ""
+}
+
+const chaptersObj = {
+    slug: "chapitre_1",
+    slug: "chapitre_2",
+    slug: "chapitre_3",
+    slug: "chapitre_4"
+}
+
+function goToChapter(chapterName) {
+
+}
