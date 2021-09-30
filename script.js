58c0e726bede24d282686e50aec6dbbce95b22d4
Snxww <90851816+Snxww@users.noreply.github.com>
2021-09-29 21:06:40 -0400

    changed spider ambush picture + Chapitre 1 et 2

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const chapitre1 = {
-    subtitle: "titreChapitre1",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    img: ""
+    subtitle: "Premiers pas",
+    text: "En entrant dans la mine abandonnée, vous rencontrez très tôt une bifurquation du chemin principal. Vous pouvez soit emprunter le tunnel se dirigeant vers la gauche soit prendre celui droite.",
+    img: "assets/cave_split"
 }
 
 const chapitre2 = {
-    subtitle: "titreChapitre2",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    img: ""
+    subtitle: "Embuscade",
+    text: "Au bas de l'escalier délabré, vous remarquez un changement au niveau de l'humidité de l'air. Lanterne à la main, vous éclairez le nouveau tunnel se présentant devant vous. Quelques minutes plus tard, le passage débouche enfin sur une très grande pièce ayant la forme d'un dome. Du sol sont émergées des tours de pierres portant, encastrées dans celles-ci, des pierres précieuses émanant une douce lueur verte. Au loin, vous apercevez différents tunnels à partir de l'entrée de la pièce. Tout semblait paisible alors que vous vous dirigiez vers le centre de la pièce, quand soudain, vous entendez un cliquetis qui vous glace les entrailles. Une araignée géante et poilue descend rapidement de sa toile devant vous et vous prend en embuscade! Vous apercevez un tunnel à votre gauche, puis un à votre droite. Vous vous faites aussi l'idée de lancer votre lanterne sur l'araignée pour tenter de la brûler ou bien de l'affronter à l'aide de votre épée.",
+    img: "assets/spider_ambush"
 }
 
 const chapitre3 = {
-    subtitle: "titreChapitre3",
+    subtitle: "Décision",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    img: ""
+    img: "assets/adventurer_after_combat"
 }
 
 const chapitre4 = {
-    subtitle: "titreChapitre4",
+    subtitle: "Repaire de l'araignée",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    img: ""
+    img: "assets/spider_nest"
 }
 
 const chaptersObj = {
